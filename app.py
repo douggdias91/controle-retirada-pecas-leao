6eedf2729494c2c99fd9f141e17eac9d0c95a766
douggdias91 <douglas.gouveia.dias@hotmail.com>
2025-08-01 08:01:01 -0300

    Update app.py

--- app.py
+++ app.py
@@ -67,5 +67,8 @@ def add_peca():
         writer.writerow([request.form['codigo'], request.form['descricao']])
     return redirect('/admin')
 
+import os
+
 if __name__ == "__main__":
     app.run(host="0.0.0.0", port=int(os.environ.get("PORT", 5000)))
+
